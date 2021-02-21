dcea749021b7c59a52a112470bef8f01cc1a36d5
Johannes Bensmann <johannesbensmann@gmx.de>
2021-02-21 13:23:44 +0100

    applied focusonclick patch

--- config.h
+++ config.h
@@ -7,6 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int horizpadbar        = 10;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 8;        /* vertical padding for statusbar */
+static const int focusonwheel       = 0;        /* from the focusonclick patch */
 static const char *fonts[]          = { "FontAwesome5Free:pixelsize=13:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
