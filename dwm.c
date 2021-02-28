1e28c4479669a5318c3f2d66a8431009a499a324
Johannes Bensmann <johannesbensmann@gmx.de>
2021-02-28 09:52:37 +0100

    changed initial layout to monocle

--- dwm.c
+++ dwm.c
@@ -736,7 +736,7 @@ createmon(void)
 	m->nmaster = nmaster;
 	m->showbar = showbar;
 	m->topbar = topbar;
-	m->lt[0] = &layouts[0];
+	m->lt[0] = &layouts[2];
 	m->lt[1] = &layouts[1 % LENGTH(layouts)];
 	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
 
