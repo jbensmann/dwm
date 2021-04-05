8c75ae1b0aebf1705e994f314b97aac06ca7ce1b
Johannes Bensmann <johannesbensmann@gmx.de>
2021-04-05 11:13:43 +0200

    replaced browser scratchpad with a cht.sh shell

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title              tags mask     isfloating   monitor   scratch key */
 	{ NULL,       NULL,   "scratchpad",          0,            1,           -1,       's' },
-	{ NULL,       NULL,   "qutebrowser",  0,            1,           -1,       'w' },
+	{ NULL,       NULL,   "cht.sh",              0,            1,           -1,       'w' },
 };
 
 /* layout(s) */
@@ -65,14 +65,14 @@ static const char *termcmd[]  = { "st", "tmux", NULL };
 
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd[] = {"s", "st", "-t", "scratchpad", "-g", "120x40", "-A", "0.85", "tmux", NULL}; 
-static const char *qutebrowsercmd[] = {"w", "qutebrowser", NULL};
+static const char *chtshcmd[] =      {"w", "st", "-t", "cht.sh",     "-g", "120x40", "-A", "0.85", "tmux", "-c", "cht.sh --shell", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_w,      togglescratch,  {.v = qutebrowsercmd } },
+	{ MODKEY,                       XK_w,      togglescratch,  {.v = chtshcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
