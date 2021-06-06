872422f6d55a9a4f287acca0708422e7d7588da8
Johannes Bensmann <johannesbensmann@gmx.de>
2021-06-06 09:39:57 +0200

    added transparent bar

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ This is my build of dwm, a minimalist tiling window manager.
 - layouts per tag
 - only show active tags
 - fancybar: shows all active applications in the status bar
-- cycle through tags
 - real fullscreen mode
 - a scratchpad terminal
+- transparent bar
 - some other minor tweaks
 
 ## Non-Features
--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int baralpha  = 0xa0;
+static const unsigned int borderalpha = 0xa0;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -17,8 +19,13 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]       = "#506050";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, "#253030", col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
