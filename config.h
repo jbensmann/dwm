caa936a00c1e6df1ecf436f121d287c6d2165286
Johannes Bensmann <johannesbensmann@gmx.de>
2023-07-23 11:27:29 +0200

    add deck layout

--- config.h
+++ config.h
@@ -55,6 +55,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[D]",      deck },
 };
 
 /* key definitions */
@@ -71,14 +72,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "combi" };
-static const char *termcmd[]  = { "alacritty", "-e", "tmux", NULL };
+static const char *termcmd[]  = { "wezterm", "-e", "tmux", NULL };
 /* static const char *termcmd[]  = { "st", "tmux", NULL }; */
 
 /*First arg only serves to match against key in rules*/
-static const char *scratchpadcmd[] = {"s", "alacritty", "-t", "scratchpad", "-e", "tmux", NULL}; 
-static const char *sgptcmd[] =      {"w", "alacritty", "-t", "sgpt",       "-e", "tmux", "-c", "sgpt --temperature 0.4 --repl temp", NULL};
-static const char *transencmd[] =    {"t", "alacritty", "-t", "transen",    "-e", "tmux", "-c", "trans en:de -v -I -theme random", NULL};
-static const char *transdecmd[] =    {"d", "alacritty", "-t", "transde",    "-e", "tmux", "-c", "trans de:en -v -I -theme random", NULL};
+static const char *scratchpadcmd[] = {"s", "wezterm", "-t", "scratchpad", "-e", "tmux", NULL}; 
+static const char *sgptcmd[] =      {"w", "wezterm", "-t", "sgpt",       "-e", "tmux", "-c", "sgpt --temperature 0.4 --repl temp", NULL};
+static const char *transencmd[] =    {"t", "wezterm", "-t", "transen",    "-e", "tmux", "-c", "trans en:de -v -I -theme random", NULL};
+static const char *transdecmd[] =    {"d", "wezterm", "-t", "transde",    "-e", "tmux", "-c", "trans de:en -v -I -theme random", NULL};
 /* static const char *scratchpadcmd[] = {"s", "st", "-t", "scratchpad", "-g", "120x40", "-A", "0.85", "tmux", NULL}; */ 
 /* static const char *chtshcmd[] =      {"w", "st", "-t", "cht.sh",     "-g", "120x40", "-A", "0.85", "tmux", "-c", "cht.sh --shell", NULL}; */
 
@@ -107,6 +108,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_e,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_e,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
